d0d289962d30f6f9bf5f6d27ac81e1b455d0bd2d
Kamenev Nikolay <79969997909@mail.ru>
2021-07-25 17:52:06 +0300

    added protection for routes

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -5,11 +5,12 @@ import {
   Get,
   Param,
   Post,
-  Put,
+  Put, UseGuards,
 } from '@nestjs/common';
 import { BookDto } from './dto/book.dto';
 import { BooksService } from './books.service';
 import { Book } from './schemas/book.schema';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('books')
 export class BooksController {
@@ -25,11 +26,13 @@ export class BooksController {
     return this.bookService.getById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createdBookDto: BookDto): Promise<Book> {
     return this.bookService.create(createdBookDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(
     @Body() updateBookDto: BookDto,
@@ -38,6 +41,7 @@ export class BooksController {
     return this.bookService.update(id, updateBookDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string): Promise<Book> {
     return this.bookService.remove(id);
