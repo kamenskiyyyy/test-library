25f62475160f5ba4245404dd2876b61f196745d2
Kamenev Nikolay <79969997909@mail.ru>
2021-07-26 15:10:25 +0300

    added validation for books

--- src/books/dto/book.dto.ts
+++ src/books/dto/book.dto.ts
@@ -1,7 +1,28 @@
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+  Length,
+  MinLength,
+} from 'class-validator';
+
 export class BookDto {
-  readonly title: string;
-  readonly description: string;
-  readonly price: number;
-  readonly author: string;
-  readonly year: number;
+  @IsString()
+  @Length(1, 50, { message: 'Длинна заголовка может быть от 1 до 50 символов' })
+  title: string;
+
+  @IsOptional()
+  description: string;
+
+  @IsNumber()
+  @MinLength(1, { message: 'Обязательно укажите стоимость книги' })
+  price: number;
+
+  @IsOptional()
+  @IsString()
+  author: string;
+
+  @IsOptional()
+  @IsNumber()
+  year: number;
 }
